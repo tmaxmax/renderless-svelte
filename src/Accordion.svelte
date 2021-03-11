808a9326fba8fa88e69bb34083047cc7e26d4711
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-11 15:14:39 +0200

    Migrate Accordion and AccordionGroup to Typescript

--- src/Accordion.svelte
+++ src/Accordion.svelte
@@ -1,18 +1,35 @@
-<script>
-	import { getContext } from 'svelte'
-	export let isOpen = false
-	
-	const close = () => isOpen = false
-	const toggle = () => isOpen = !isOpen
-    
-    const context = getContext('Accordion')
-	const setCurrent = context && context.setCurrent
-	
-	$: isOpen && setCurrent && setCurrent(close)
-	
+<script lang="ts">
+  import { contextKey } from './AccordionGroup.svelte'
+  import type { Context } from './AccordionGroup.svelte'
+
+  import { getContext } from 'svelte'
+
+  /**
+   * Flag that indicates if the accordion is open.
+   */
+  export let isOpen = false
+
+  const close = () => {
+    isOpen = false
+  }
+  /**
+   * Opens or closes the accordion. Use it to toggle
+   * the accordion state programatically (e.g. as a handler for event listeners).
+   */
+  const toggle = () => {
+    isOpen = !isOpen
+  }
+
+  // Type this with undefined because if the context doesn't exist
+  // getContext returns undefined.
+  const context = getContext<Context | undefined>(contextKey)
+
+  $: if (isOpen) {
+    context?.setAccordion(close)
+  }
 </script>
 
 <slot name="header" {toggle} {isOpen} />
 {#if isOpen}
-	<slot />
+  <slot />
 {/if}
--- src/AccordionGroup.svelte
+++ src/AccordionGroup.svelte
@@ -1,15 +1,44 @@
-<script>
-	import { setContext } from 'svelte'
-	import { writable } from 'svelte/store'
-	
-	let current = writable(false)
-	
-	setContext('Accordion', {
-		setCurrent: fn => {
-			$current && $current !== fn && $current()
-			current.set(fn)
-		}
-	})
+<script context="module" lang="ts">
+  import key from './key'
+  /**
+   * Use this to get the AccordionGroup context.
+   */
+  export const contextKey = key('Accordion')
+
+  /**
+   * Function that closes an accordion.
+   */
+  export type Closer = () => void
+
+  /**
+   * The AccordionGroup context.
+   */
+  export interface Context {
+    /**
+     * Sets the current opened accordion for the group,
+     * by providing a function that closes it.
+     *
+     * @param fn The function that closes the accordion,
+     */
+    setAccordion(fn: Closer): void
+  }
+</script>
+
+<script lang="ts">
+  import { setContext } from 'svelte'
+  import { writable } from 'svelte/store'
+
+  let closer = writable<Closer | undefined>(undefined)
+
+  setContext<Context>(contextKey, {
+    setAccordion(fn) {
+      if ($closer === fn) {
+        return
+      }
+      $closer?.()
+      $closer = fn
+    },
+  })
 </script>
 
 <slot />
