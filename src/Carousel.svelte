b630aba771bb932fe2a44ec4641a2ce32c6247d9
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-11 15:15:06 +0200

    Migrate Carousel to Typescript

--- src/Carousel.svelte
+++ src/Carousel.svelte
@@ -1,23 +1,69 @@
-<script> 
-	export let items = []
-	export let currentIndex = 0
-	export let loop = false
-	export const setIndex = val => val < items.length && (currentIndex = val)
-	export const controls = {
-		next: () => 
-			currentIndex = currentIndex < items.length - 1 
-				? currentIndex + 1 
-				: loop
-				? 0
-				: items.length - 1,
-		previous: () => 
-			currentIndex = currentIndex != 0 
-				? currentIndex - 1 
-				: loop
-				? items.length - 1
-				: 0
-	}
-	$: payload = items[currentIndex]
+<script context="module" lang="ts">
+  export interface CarouselControls {
+    /**
+     * Advances the carousel to the next item.
+     */
+    next(): void
+    /**
+     * Iterates the carousel back to the previos item.
+     */
+    previous(): void
+  }
 </script>
 
-<slot {currentIndex} {payload} {setIndex} {controls} {loop}></slot>
+<script lang="ts">
+  /**
+   * The items to create the carousel with.
+   */
+  export let items: any[]
+  /**
+   * The index of the current carousel item.
+   */
+  export let currentIndex = 0
+  /**
+   * Indicates whether the carousel should loop back to the beginning
+   * if iterated further the last element.
+   *
+   * @default false
+   */
+  export let loop = false
+
+  /**
+   * Sets the current carousel item to the ith item
+   * in the provided items.
+   *
+   * @param i The index of the item.
+   */
+  export const setIndex = (i: number) => {
+    if (i < items.length) {
+      currentIndex = i
+    }
+  }
+
+  /**
+   * Provides functions to loop through the carousel items.
+   */
+  export const controls: CarouselControls = {
+    next() {
+      if (currentIndex < items.length - 1) {
+        currentIndex++
+      } else if (loop) {
+        currentIndex = 0
+      }
+    },
+    previous: () => {
+      if (currentIndex > 0) {
+        currentIndex--
+      } else if (loop) {
+        currentIndex = items.length - 1
+      }
+    },
+  }
+
+  /**
+   * The current item in the carousel.
+   */
+  $: payload = items[currentIndex]
+</script>
+
+<slot {currentIndex} {payload} {setIndex} {controls} {loop} />
