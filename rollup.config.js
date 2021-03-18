99394d21b2825e4fad1e677424b4edfe92ee0c3b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-18 17:25:18 +0200

    Update build configuration and add svelte-check

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,39 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import pkg from './package.json'
+import typescript from '@rollup/plugin-typescript'
+
+const { preprocess } = require('./svelte.config')
+
+const dev = process.env.NODE_ENV === 'development'
+const sourcemap = dev ? 'inline' : false
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase())
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
-	],
-	plugins: [
-		svelte(),
-		resolve()
-	]
-};
+  input: 'src/index.ts',
+  output: [
+    { file: pkg.module, format: 'es', sourcemap },
+    { file: pkg.main, format: 'umd', name, sourcemap },
+  ],
+  plugins: [
+    svelte({
+      compilerOptions: {
+        dev,
+      },
+      emitCss: true,
+      preprocess,
+    }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    typescript({
+      noEmitOnError: !dev,
+      sourceMap: !!sourcemap,
+    }),
+  ],
+}
