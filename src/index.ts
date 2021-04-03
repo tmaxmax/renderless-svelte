eb7d37e430d4911cccb47a17d2ec21f8a465c346
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-03 15:19:01 +0300

    Remove entrypoint

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Accordion } from './Accordion.svelte'
-export { default as AccordionGroup } from './AccordionGroup.svelte'
-export { default as Carousel, CarouselControls } from './Carousel.svelte'
-export { default as Notification, notifications } from './Notifications.svelte'
-export { default as Modal, closeModal, openModal } from './Modal.svelte'
-export { default as TabControl } from './TabControl.svelte'
-export { default as TabControlItem } from './TabControlItem.svelte'
-export { default as Tooltip, tooltip } from './Tooltip.svelte'
